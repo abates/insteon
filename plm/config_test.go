6c04227aa82bb9ad7d9e8fc22b7e65f4258f28b1
Andrew Bates <abates@omeganetserv.com>
2018-01-07 19:46:25 -0500

    Testing...

--- plm/config_test.go
+++ plm/config_test.go
@@ -2,7 +2,7 @@ package plm
 
 import "testing"
 
-func TestSettingFlags(t *testing.T) {
+func TestSettingConfigFlags(t *testing.T) {
 	config := Config(0x00)
 
 	tests := []struct {
@@ -34,6 +34,58 @@ func TestSettingFlags(t *testing.T) {
 		test.clearer()
 		if byte(config) != 0x00 {
 			t.Errorf("tests[%d] expected 0x00 got 0x%02x", i, byte(config))
+		}
+	}
+}
+
+func TestConfigString(t *testing.T) {
+	tests := []struct {
+		input    byte
+		expected string
+	}{
+		{0x80, "L..."},
+		{0x40, ".M.."},
+		{0x20, "..A."},
+		{0x10, "...D"},
+		{0xf0, "LMAD"},
+	}
+
+	for i, test := range tests {
+		config := Config(test.input)
+		if config.String() != test.expected {
+			t.Errorf("tests[%d] expected %q got %q", i, test.expected, config.String())
+		}
+	}
+}
+
+func TestConfigMarshalUnmarshal(t *testing.T) {
+	tests := []struct {
+		input byte
+	}{
+		{0x00},
+		{0x80},
+		{0x40},
+		{0x20},
+		{0x10},
+		{0xf0},
+	}
+
+	var config Config
+	err := config.UnmarshalBinary([]byte{})
+	if err == nil {
+		t.Errorf("Expected error, got nil")
+	}
+
+	for i, test := range tests {
+		config.UnmarshalBinary([]byte{test.input})
+
+		if byte(config) != test.input {
+			t.Errorf("tests[%d] expected 0x%02x got 0x%02x", i, test.input, config)
+		}
+
+		buf, _ := config.MarshalBinary()
+		if buf[0] != test.input {
+			t.Errorf("tests[%d] expected 0x%02x got 0x%02x", i, test.input, buf[0])
 		}
 	}
 }
