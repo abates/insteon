83742349e4c75b8db80e94bd35c3bef9e72c5e5b
Andrew Bates <abates@omeganetserv.com>
2021-09-23 10:44:33 -0400

    Removed mutexes
    Database locking should be provided by the consumer
    of the database, not the database itself

--- db/database.go
+++ db/database.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"io"
 	"io/ioutil"
-	"sync"
 
 	"github.com/abates/insteon"
 )
@@ -28,8 +27,6 @@ func open(db Database, bus insteon.Bus, dst insteon.Address) (device insteon.Dev
 			if err == nil {
 				db.Put(info)
 			}
-		} else if err == insteon.ErrNotLinked {
-			device, _ = insteon.Create(bus, insteon.DeviceInfo{Address: dst, EngineVersion: insteon.VerI2Cs})
 		}
 	}
 	return device, err
@@ -54,6 +51,10 @@ type Database interface {
 	// Put will store the DeviceInfo object in the Database overwriting
 	// any existing object
 	Put(info insteon.DeviceInfo)
+
+	// Filter will return a list of addresses that match the
+	// given device categories
+	Filter(domains ...insteon.Domain) []insteon.Address
 
 	// Open will return an initialized Insteon device object.  If the
 	// DeviceInfo object does not exist in the database, then the database
@@ -91,21 +92,25 @@ func NewMemDB() Database {
 }
 
 type memDB struct {
-	lock   sync.Mutex
 	values map[insteon.Address]insteon.DeviceInfo
 	dirty  bool
 }
 
+func (db *memDB) Filter(domains ...insteon.Domain) (matches []insteon.Address) {
+	for addr, info := range db.values {
+		if info.DevCat.In(domains...) {
+			matches = append(matches, addr)
+		}
+	}
+	return matches
+}
+
 func (db *memDB) Get(addr insteon.Address) (insteon.DeviceInfo, bool) {
-	db.lock.Lock()
-	defer db.lock.Unlock()
 	info, found := db.values[addr]
 	return info, found
 }
 
 func (db *memDB) Put(info insteon.DeviceInfo) {
-	db.lock.Lock()
-	defer db.lock.Unlock()
 	if old := db.values[info.Address]; old != info {
 		db.dirty = true
 		db.values[info.Address] = info
@@ -125,8 +130,6 @@ func (db *memDB) Load(reader io.Reader) error {
 }
 
 func (db *memDB) NeedsSaving() bool {
-	db.lock.Lock()
-	defer db.lock.Unlock()
 	return db.dirty
 }
 
@@ -135,23 +138,17 @@ func (db *memDB) Save(writer io.Writer) error {
 	if err == nil {
 		_, err = writer.Write(buf)
 		if err == nil {
-			db.lock.Lock()
 			db.dirty = false
-			db.lock.Unlock()
 		}
 	}
 	return err
 }
 
 func (db *memDB) MarshalJSON() ([]byte, error) {
-	db.lock.Lock()
-	defer db.lock.Unlock()
 	return json.Marshal(db.values)
 }
 
 func (db *memDB) UnmarshalJSON(data []byte) error {
-	db.lock.Lock()
-	defer db.lock.Unlock()
 	db.values = make(map[insteon.Address]insteon.DeviceInfo)
 	return json.Unmarshal(data, &db.values)
 }
