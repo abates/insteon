6f1ebdf01f33256359ed11501e98e5548bba3411
Andrew Bates <abates@omeganetserv.com>
2021-09-23 10:40:38 -0400

    Updated copyright year

--- commands.go
+++ commands.go
@@ -1,4 +1,4 @@
-// Copyright 2020 Andrew Bates
+// Copyright 2021 Andrew Bates
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
