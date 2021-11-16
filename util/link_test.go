f4567dcdd914b5a5f365515cffdabf3d4ff41ade
Andrew Bates <abates@omeganetserv.com>
2021-11-15 20:57:45 -0500

    Adding more tests

--- util/link_test.go
+++ util/link_test.go
@@ -16,7 +16,9 @@ import (
 )
 
 type testLinkable struct {
-	links []insteon.LinkRecord
+	links    []insteon.LinkRecord
+	name     string
+	commands chan<- string
 }
 
 type testWritableLinkable struct {
@@ -34,34 +36,6 @@ func (twl *testWritableLinkable) WriteLink(i int, link insteon.LinkRecord) error
 	return nil
 }
 
-func (tl *testLinkable) Load(filename string) (meta []string, want string, err error) {
-	inputStr, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return
-	}
-
-	for _, line := range strings.Split(string(inputStr), "\n") {
-		if strings.HasPrefix(line, "#") {
-			meta = append(meta, strings.TrimSpace(strings.TrimPrefix(line, "#")))
-		}
-	}
-
-	tl.links, err = TextToLinks(string(inputStr))
-	if err == nil {
-		ext := filepath.Ext(filename)
-		wantFile := filename[0:len(filename)-len(ext)] + ".want"
-		var wantBytes []byte
-		wantBytes, err = ioutil.ReadFile(wantFile)
-		want = string(wantBytes)
-		if errors.Is(err, fs.ErrNotExist) {
-			err = nil
-		} else if err != nil {
-			err = fmt.Errorf("Failed reading %s %w", wantFile, err)
-		}
-	}
-	return
-}
-
 func (tl *testLinkable) Address() insteon.Address {
 	return insteon.Address(0x010203)
 }
@@ -77,79 +51,114 @@ func (tl *testLinkable) WriteLinks(links ...insteon.LinkRecord) error {
 }
 
 func (tl *testLinkable) UpdateLinks(...insteon.LinkRecord) error { return nil }
-func (tl *testLinkable) EnterLinkingMode(insteon.Group) error    { return nil }
-func (tl *testLinkable) EnterUnlinkingMode(insteon.Group) error  { return nil }
-func (tl *testLinkable) ExitLinkingMode() error                  { return nil }
 
-func TestFixCrosslinks(t *testing.T) {
-	inputs, _ := filepath.Glob("testdata/fixcrosslink_*.input")
-	for _, inputFile := range inputs {
-		t.Run(filepath.Base(inputFile), func(t *testing.T) {
-			tl := &testLinkable{}
-			meta, wantStr, err := tl.Load(inputFile)
-			want, _ := TextToLinks(wantStr)
-			if err == nil {
-				addresses := []insteon.Address{}
-				for _, str := range meta {
-					address := insteon.Address(0)
-					address.Set(str)
-					addresses = append(addresses, address)
+func (tl *testLinkable) EnterLinkingMode(insteon.Group) error {
+	tl.commands <- fmt.Sprintf("%s EnterLinkingMode", tl.name)
+	return nil
+}
+
+func (tl *testLinkable) EnterUnlinkingMode(insteon.Group) error {
+	tl.commands <- fmt.Sprintf("%s EnterUnLinkingMode", tl.name)
+	return nil
+}
+
+func (tl *testLinkable) ExitLinkingMode() error {
+	tl.commands <- fmt.Sprintf("%s ExitLinkingMode", tl.name)
+	return nil
+}
+
+func TestLinkUtils(t *testing.T) {
+	tests := []struct {
+		name   string
+		prefix string
+		test   func(input []insteon.LinkRecord, meta []string, want string, t *testing.T)
+	}{
+		{"Fix Cross Links", "testdata/fixcrosslink_", testFixCrosslinks},
+		{"Add Links", "testdata/addlinks_", testAddLinks},
+		{"Print Links", "testdata/printlinks", testPrintLinks},
+		{"Dump Links", "testdata/dumplinks", testDumpLinks},
+		{"Find Links", "testdata/findlinkrecord", testFindLinkRecord},
+		{"Anonymize", "testdata/anonymize", testAnonymize},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			inputs, _ := filepath.Glob(fmt.Sprintf("%s*.input", test.prefix))
+			for _, inputFile := range inputs {
+				inputBytes, err := ioutil.ReadFile(inputFile)
+				if err != nil {
+					t.Fatalf("Failed to read input file %v", err)
 				}
 
-				got := fixCrosslinks(tl.links, addresses...)
-				if !reflect.DeepEqual(want, got) {
-					t.Errorf("Wanted\n%s got\n%s", LinksToText(want, false), LinksToText(got, false))
+				input, err := TextToLinks(string(inputBytes))
+				if err != nil {
+					t.Fatalf("Failed to convert %s to links %v", inputFile, err)
 				}
-			} else {
-				t.Errorf("Unexpected error %v", err)
+
+				meta := []string{}
+				for _, line := range strings.Split(string(inputBytes), "\n") {
+					if strings.HasPrefix(line, "#") {
+						meta = append(meta, strings.TrimSpace(strings.TrimPrefix(line, "#")))
+					}
+				}
+
+				ext := filepath.Ext(inputFile)
+				wantFile := inputFile[0:len(inputFile)-len(ext)] + ".want"
+				want, err := ioutil.ReadFile(wantFile)
+				if err != nil && !errors.Is(err, fs.ErrNotExist) {
+					t.Fatalf("Failed to read want file %v", err)
+				}
+
+				test.test(input, meta, string(want), t)
 			}
 		})
 	}
 }
 
-func TestAddLinks(t *testing.T) {
-	inputs, _ := filepath.Glob("testdata/addlinks_*.input")
-	for _, inputFile := range inputs {
-		t.Run(filepath.Base(inputFile), func(t *testing.T) {
-			tl := &testLinkable{}
-			meta, wantStr, err := tl.Load(inputFile)
-			twl := &testWritableLinkable{&testLinkable{}}
-			twl.links = make([]insteon.LinkRecord, len(tl.links))
-			copy(twl.links, tl.links)
+func testFixCrosslinks(input []insteon.LinkRecord, meta []string, wantStr string, t *testing.T) {
+	tl := &testLinkable{links: input}
+	want, _ := TextToLinks(wantStr)
+	addresses := Addresses{}
+	addresses.Set(meta)
 
-			want := []insteon.LinkRecord{}
-			addlinks := []insteon.LinkRecord{}
-			if err == nil {
-				for _, m := range meta {
-					l := insteon.LinkRecord{}
-					err = l.UnmarshalText([]byte(m))
-					if err != nil {
-						break
-					}
-					addlinks = append(addlinks, l)
-				}
-			}
+	got := fixCrosslinks(tl.links, addresses...)
+	if !reflect.DeepEqual(want, got) {
+		t.Errorf("Wanted\n%s got\n%s", LinksToText(want, false), LinksToText(got, false))
+	}
+}
 
-			if err == nil {
-				want, err = TextToLinks(wantStr)
-			}
+func testAddLinks(input []insteon.LinkRecord, meta []string, wantStr string, t *testing.T) {
+	tl := &testLinkable{links: input}
+	twl := &testWritableLinkable{&testLinkable{}}
+	twl.links = make([]insteon.LinkRecord, len(tl.links))
+	copy(twl.links, tl.links)
 
-			if err == nil {
-				for _, l := range []devices.Linkable{tl, twl} {
-					err := AddLinks(l, addlinks...)
-					if err == nil {
-						got, _ := l.Links()
-						if !reflect.DeepEqual(want, got) {
-							t.Errorf("%T: Wanted\n%s got\n%s", l, LinksToText(want, false), LinksToText(got, false))
-						}
-					} else {
-						t.Errorf("Unexpected error %v", err)
-					}
-				}
-			} else {
-				t.Errorf("Unexpected error %v", err)
+	want := []insteon.LinkRecord{}
+	addlinks := []insteon.LinkRecord{}
+	for i, m := range meta {
+		l := insteon.LinkRecord{}
+		err := l.UnmarshalText([]byte(m))
+		if err != nil {
+			t.Fatalf("Failed to unmarshal meta record %d: %v", i, err)
+		}
+		addlinks = append(addlinks, l)
+	}
+
+	want, err := TextToLinks(wantStr)
+	if err != nil {
+		t.Fatalf("Failed to convert want string to links: %v", err)
+	}
+
+	for _, l := range []devices.Linkable{tl, twl} {
+		err := AddLinks(l, addlinks...)
+		if err == nil {
+			got, _ := l.Links()
+			if !reflect.DeepEqual(want, got) {
+				t.Errorf("%T: Wanted\n%s got\n%s", l, LinksToText(want, false), LinksToText(got, false))
 			}
-		})
+		} else {
+			t.Errorf("Unexpected error %v", err)
+		}
 	}
 }
 
@@ -234,84 +243,74 @@ UR        1 04.05.06   00 00 00
 	}
 }
 
-func TestFindLinkRecord(t *testing.T) {
-	tests := []struct {
-		desc    string
-		input   insteon.LinkRecord
-		want    insteon.LinkRecord
-		wantErr error
-	}{
-		{"found", insteon.ControllerLink(1, insteon.Address(0x010203)), insteon.ControllerLink(1, insteon.Address(0x010203)), nil},
-		{"not found", insteon.ControllerLink(1, insteon.Address(0x070809)), insteon.LinkRecord{}, ErrLinkNotFound},
-	}
-
-	for _, test := range tests {
-		t.Run(test.desc, func(t *testing.T) {
-			tl := &testLinkable{}
-			_, _, err := tl.Load("testdata/findlinkrecord.input")
-			if err == nil {
-				input := test.input
-				got, err := FindLinkRecord(tl, input.Flags.Controller(), input.Address, input.Group)
-				if err == test.wantErr {
-					if test.want != got {
-						t.Errorf("want link %x got %x", byte(test.want.Flags), byte(got.Flags))
-					}
-				} else {
-					t.Errorf("Wanted error %v got %v", test.wantErr, err)
-				}
-			} else {
-				t.Errorf("Unexpected error %v", err)
-			}
-		})
+func testAnonymize(input []insteon.LinkRecord, meta []string, wantStr string, t *testing.T) {
+	addresses := Addresses{}
+	addresses.Set(meta)
+	want, err := TextToLinks(wantStr)
+	if err == nil {
+		got := Anonymize(input, addresses...)
+		if !reflect.DeepEqual(want, got) {
+			t.Errorf("Wanted\n%v\ngot\n%v\n", LinksToText(want, false), LinksToText(got, false))
+		}
+	} else {
+		t.Errorf("Unexpected error %v", err)
 	}
 }
 
-func TestPrintLinks(t *testing.T) {
-	inputs, _ := filepath.Glob("testdata/printlinks*.input")
-	for _, input := range inputs {
-		t.Run(filepath.Base(input), func(t *testing.T) {
+func testFindLinkRecord(input []insteon.LinkRecord, meta []string, _ string, t *testing.T) {
+	tl := &testLinkable{links: input}
+	for i, str := range meta {
+		data := strings.Split(str, ":")
+		wantFound := true
+		if data[0] == "found" {
+			wantFound = true
+		} else if data[0] == "not_found" {
+			wantFound = false
+		} else {
+			t.Fatalf("Unknown operation %q", data[0])
+		}
 
-			tl := &testLinkable{}
-			_, want, err := tl.Load(input)
-			if err == nil {
-				out := &bytes.Buffer{}
-				err = PrintLinkDatabase(out, tl)
-				if err == nil {
-					got := string(out.Bytes())
-					if want != got {
-						t.Errorf("Wanted:\n%v\nGot:\n%v", want, got)
-					}
-				} else {
-					t.Fatalf("Unexpected error: %v", err)
-				}
-			} else {
-				t.Fatalf("Unexpected error: %v", err)
+		want := insteon.LinkRecord{}
+		err := want.UnmarshalText([]byte(data[1]))
+		if err == nil {
+			got, err := FindLinkRecord(tl, want.Flags.Controller(), want.Address, want.Group)
+			if wantFound && err != nil {
+				t.Errorf("Wanted record to be found got %v", err)
+			} else if !wantFound && err == nil {
+				t.Errorf("Wanted record to not be found, but %v was found", got)
+			} else if err == nil && want != got {
+				t.Errorf("want link %v got %v", want, got)
 			}
-		})
+		} else {
+			t.Fatalf("Failed to unmarshal link record %d: %v", i, err)
+		}
 	}
 }
 
-func TestDumpLinks(t *testing.T) {
-	inputs, _ := filepath.Glob("testdata/dumplinks*.input")
-	for _, input := range inputs {
-		t.Run(filepath.Base(input), func(t *testing.T) {
+func testPrintLinks(input []insteon.LinkRecord, meta []string, want string, t *testing.T) {
+	tl := &testLinkable{links: input}
+	out := &bytes.Buffer{}
+	err := PrintLinkDatabase(out, tl)
+	if err == nil {
+		got := string(out.Bytes())
+		if want != got {
+			t.Errorf("Wanted:\n%v\nGot:\n%v", want, got)
+		}
+	} else {
+		t.Fatalf("Unexpected error: %v", err)
+	}
+}
 
-			tl := &testLinkable{}
-			_, want, err := tl.Load(input)
-			if err == nil {
-				out := &bytes.Buffer{}
-				err = DumpLinkDatabase(out, tl)
-				if err == nil {
-					got := string(out.Bytes())
-					if want != got {
-						t.Errorf("Wanted:%q\nGot:%q", want, got)
-					}
-				} else {
-					t.Fatalf("Unexpected error: %v", err)
-				}
-			} else {
-				t.Fatalf("Unexpected error: %v", err)
-			}
-		})
+func testDumpLinks(input []insteon.LinkRecord, meta []string, want string, t *testing.T) {
+	tl := &testLinkable{links: input}
+	out := &bytes.Buffer{}
+	err := DumpLinkDatabase(out, tl)
+	if err == nil {
+		got := string(out.Bytes())
+		if want != got {
+			t.Errorf("Wanted:%q\nGot:%q", want, got)
+		}
+	} else {
+		t.Fatalf("Unexpected error: %v", err)
 	}
 }
